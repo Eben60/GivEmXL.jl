3f5e5b9920564d03a5a491a3dc3216ec717c6b9c
Ben Elkin <8xe8n-cm@online.de>
2024-03-20 13:47:55 +0100

    using SimpleArgParse - WIP

--- src/parse_ARGS.jl
+++ src/parse_ARGS.jl
@@ -1,18 +1,18 @@
-using ArgParse
+using SimpleArgParse
 
-function parse_commandline()
-    s = ArgParseSettings()
+# function parse_commandline()
+#     s = ArgParseSettings()
 
-    @add_arg_table! s begin
-        "--plottype", "-p"
-            help = "plot type (must be accepted by Plots.jl)"
-            arg_type = String
-            default = "PNG"
-        "--otherargs", "-o"
-            help = "another option with an argument"
-    end
+#     @add_arg_table! s begin
+#         "--plottype", "-p"
+#             help = "plot type (must be accepted by Plots.jl)"
+#             arg_type = String
+#             default = "PNG"
+#         "--otherargs", "-o"
+#             help = "another option with an argument"
+#     end
 
-    return parse_args(s)
-end
+#     return parse_args(s)
+# end
 
 to_nt(d) = NamedTuple([(Symbol(k) => v) for (k,v) in d if !isnothing(v)])
