11d15525d2e092a9f9aa356969c20533a2ccc1f1
Ben Elkin <8xe8n-cm@online.de>
2024-03-23 16:15:03 +0100

    move functions into package

--- src/GivEmExel.jl
+++ src/GivEmExel.jl
@@ -1,10 +1,14 @@
 module GivEmExel
 using Unitful, DataFrames, XLSX, Preferences
 using NativeFileDialog: pick_file
+using SimpleArgParse
 
 export pick_file, @load_preference, @set_preferences!, @has_preference
 export parse_cl_string
 export repl
+export emptyargs, prompt_and_parse, proc_ARGS, full_interact
+
+export SimpleArgParse
 
 # temporary exports
 export read_units, nt_skipmissing, merge_params, process_data, keys_skipmissing, merge_params, 
@@ -15,5 +19,6 @@ include("parse_ARGS.jl")
 include("utils.jl")
 include("repl.jl")
 include("get_xl.jl")
+include("interact.jl")
 
 end
--- src/prompt2.jl
+++ src/prompt2.jl
@@ -1,76 +1,9 @@
-using SimpleArgParse
 using GivEmExel
-
-using Preferences
-using NativeFileDialog: pick_file
-
-emptyargs() = Pair{Symbol, Any}[]
+using SimpleArgParse
 
 include("prompt2-init.jl")
 
-function prompt_and_parse(pp)
-    proceed = true
-    if !isnothing(pp)
-        color = pp.color
-        colorprint(pp.introduction, color)
-        colorprint(pp.prompt, color, false)
-        answer = readline()
-        cli_args = parse_cl_string(answer)
-        parse_args!(pp, cli_args)
-        ps = args_pairs(pp)
-        if get_value(pp, "--help")  
-            help(pp)
-            proceed = false
-            ps = emptyargs() # ignore other ARGS, if --help
-        end
-    else
-        ps = emptyargs()       
-    end
-    return (;proceed, argpairs = ps)
-end
 
-function proc_ARGS(pp0)
-    proceed = true
-    if !isnothing(pp0)
-        parse_args!(pp0.parser)
-        if get_value(pp0, "--help")  
-            help(pp0)
-            proceed = false
-        end
-        ps0 = args_pairs(pp0)
-    else
-        ps0 = emptyargs()
-    end
-    return (;proceed, argpairs = ps0)
-end
-
-
-function full_interact(pp0, pps)
-    allargpairs = []
-    (;proceed, argpairs) = proc_ARGS(pp0)
-    proceed || return nothing
-    push!(allargpairs, argpairs)
-
-    while true
-        (;proceed, argpairs) = prompt_and_parse(pps.gen_options)
-        if proceed
-            push!(allargpairs, argpairs)
-            break
-        end
-    end
-
-    (;proceed, argpairs) = get_xl()
-
-    proceed && push!(allargpairs, argpairs)
-    @show allargpairs
-
-
-    return nothing
-end
-
-
-# pp = testpp()
-# pp0 = makepp0()
 pps = (;gen_options)
 
 fi = full_interact(pp0, pps)
