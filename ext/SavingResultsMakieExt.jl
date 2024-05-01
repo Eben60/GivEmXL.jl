e8ff62367f9f84e2876275b38c655c066d8f3cee
Ben Elkin <8xe8n-cm@online.de>
2024-05-01 22:56:31 +0200

    minimal clean-up

--- ext/SavingResultsMakieExt.jl
+++ ext/SavingResultsMakieExt.jl
@@ -4,51 +4,11 @@ using Makie
 using GivEmExel
 import GivEmExel: isplot, save_plot
 
-MakiePlot = Union{Makie.Figure, Makie.AbstractScene, Makie.AbstractPlot}
+MakiePlot = Union{Makie.Figure, Makie.FigureAxisPlot, Makie.AbstractScene, Makie.AbstractPlot}
+# Makie.AbstractPlot actually not supported for saving by Makie
+# but that's not our problem
 
 isplot(::P ) where P <: MakiePlot = true
 save_plot(pl::P, fl) where P <: MakiePlot = Makie.save(fl, pl)
 
-"""
-FileIO.save(filename, scene; size = size(scene), pt_per_unit = 0.75, px_per_unit = 1.0)
-
-julia> supertype(Figure)
-Any
-
-julia> subtypes(Figure)
-Type[]
-
-julia> supertype(Scene)
-AbstractScene
-
-julia> supertype(AbstractScene)
-MakieCore.Transformable
-
-julia> subtypes(MakieCore.Transformable)
-3-element Vector{Any}:
- AbstractPlot
- AbstractScene
- Transformation
-
-julia> Makie.AxisPlot
-Makie.AxisPlot
-
-julia> supertype(ans)
-Any
-julia> Makie.FigureAxisPlot
-Makie.FigureAxisPlot
-
-julia> supertype(ans)
-Any 
-
-julia> Scatter
-Scatter (alias for Plot{MakieCore.scatter})
-
-julia> supertype(ans)
-MakieCore.ScenePlot{MakieCore.scatter}
-
-julia> supertype(ans)
-AbstractPlot{MakieCore.scatter}
-
-"""
 end
