96dfb289d580b8bab1dab3377627dfc26359d1e8
Ben Elkin <8xe8n-cm@online.de>
2024-07-08 10:28:56 +0200

    check/switch project

--- examples/RcExample/RcExample.jl/src/rcex.jl
+++ examples/RcExample/RcExample.jl/src/rcex.jl
@@ -1,3 +1,17 @@
+
+# preferrably you should start Julia with the corresponding project using a batch script
+# to save a second or two on environment switching on start time.
+#
+# Just for the case you didn't, we check the environment and activate the proper one if necessary.
+
+using Pkg
+
+pkg_name = "RcExample"
+
+if isnothing(Pkg.Types.Context().env.pkg) || Pkg.Types.Context().env.pkg.name != pkg_name
+    Pkg.activate(dirname(@__DIR__))
+end
+
 using RcExample
 using GivEmExel
 
