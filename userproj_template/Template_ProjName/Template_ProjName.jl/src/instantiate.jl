da1a4e000dd1e2a5933dd9aec96b0992e9b14e3a
Ben Elkin <8xe8n-cm@online.de>
2024-08-06 22:08:54 +0200

    template: adding arbitrary no of nested projects

--- userproj_template/Template_ProjName/Template_ProjName.jl/src/instantiate.jl
+++ userproj_template/Template_ProjName/Template_ProjName.jl/src/instantiate.jl
@@ -5,7 +5,15 @@ Pkg.activate(basedir)
 
 # Pkg.add(; path="/Server/InHouse_Software/AnotherInHouseProj.jl/")
 
-Pkg.develop(; path=joinpath(basedir, "included_packages/OurInHouseProj.jl/"))
+# you can safely remove the following block if you do not include any nested packages
+begin
+    incl_dir = joinpath(basedir, "included_packages/")
+    nested = filter(isdir, joinpath.(Ref(incl_dir), readdir(incl_dir)) )
+
+    for p in nested
+        Pkg.develop(; path=p)
+    end
+end
 
 Pkg.instantiate()
 ;
